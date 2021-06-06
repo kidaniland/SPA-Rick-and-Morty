fafbcb61a03c76e09bc4b5ba61f3634a27d25976
kidaniland <mdcvillas@gmail.com>
2021-06-05 22:08:44 -0400

    Modificado estilos del header

--- src/template/Header.js
+++ src/template/Header.js
@@ -6,25 +6,44 @@ const Header = () => {
                     <img src="https://media.cdn.adultswim.com/uploads/20210428/21428161947-rick-and-morty-logo-png.png">
                 </a>
             </div>
-            <div class="Header-nav">            
-                <select class="tag" name="a-z" id="selectOrder">
-                    <option value="ordenado">--Select order--</option>
-                    <option value="ascendente">A-Z</option>
-                    <option value="descendente">Z-A</option>
-                </select>
-                <div class="search">
-                    <div class="icon">
-                        <i class="fas fa-search"></i>
-                    </div>
-                    <div class="input-search">
-                    </div>
-                </div>
-                <a href="#/about/">About</a>
-                <button class="tag" id= "openPage"><i class="fas fa-bars"></i></button> 
-            </div> 
+            <div class="menu-toggle">
+                <i class="fas fa-bars"></i>
+            </div>
+            <nav class="header-nav">
+                <ul>            
+                    <li>
+                        <a class="active">
+                            <select class="tag" name="a-z" id="selectOrder">
+                                <option value="ordenado">--Select order--</option>
+                                <option value="ascendente">A-Z</option>
+                                <option value="descendente">Z-A</option>
+                            </select>
+                        </a>
+                    </li>
+                    <li>
+                        <a>
+                            <div class="search">
+                                <div class="icon">
+                                    <i class="fas fa-search"></i>
+                                </div>
+                                <div class="input-search">
+                                </div>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#/about/">About</a>
+                    </li>  
+                </ul> 
+            </nav>
+            <div class="clearfix"></div> 
         </div>
     `;
     return view
 }
 
 export default Header
+
+//<button class="tag" id= "openPage"><i class="fas fa-bars"></i></button>
+
+//https://www.youtube.com/watch?v=kEt5DCHeyJo
